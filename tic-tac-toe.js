208b8ee9d6f16b72d2499c2fdfe789a870592590
Tomek_M <tomasz.milaszewski@gmail.com>
2018-09-24 22:05:02 +0200

    alerts delayed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -27,14 +27,14 @@ document.addEventListener('click', function(event) {
         var osResult = os.join('').match(/\d+/g).sort().join('');
         var crossesResult = crosses.join('').match(/\d+/g).sort().join('');
         if (osResult.includes("012") || osResult.includes("345") || osResult.includes("678") || osResult.includes("036") || osResult.includes("147") || osResult.includes("258") || osResult.includes("048") || osResult.includes("246")) {
-            setTimeout(function() { alert('os wins'); }, 1);
+            setTimeout(function() { alert('os wins'); }, 10);
         } else {
             if (crossesResult.includes("012") || crossesResult.includes("345") || crossesResult.includes("678") || crossesResult.includes("036") || crossesResult.includes("147") || crossesResult.includes("258") || crossesResult.includes("048") || crossesResult.includes("246")) {
-                setTimeout(function() { alert('crosses wins'); }, 1);
+                setTimeout(function() { alert('crosses wins'); }, 10);
             }   
             else {
                 if (round === 9) {
-                    setTimeout(function() { alert('draw'); }, 1);
+                    setTimeout(function() { alert('draw'); }, 10);
                 }
             }
         }   
