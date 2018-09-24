179b08e72de9a8c3d20b6bebffb34e29960147d5
Tomek_M <tomasz.milaszewski@gmail.com>
2018-09-24 21:47:35 +0200

    WIP: win/lose/draw alerts

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,4 @@
-var round = 0; //os are starting
+var round = 1; //os are starting
 var crosses = [];
 var os = [];
 
@@ -22,9 +22,21 @@ document.addEventListener('click', function(event) {
     }
     // counts round number            
     round += 1;
-    console.log(crosses);
-    console.log(os);
-    var osResult = os.join('');
-    console.log(osResult);
-    
+    // winning condition
+    if (round >= 5) {
+        var osResult = os.join('').match(/\d+/g).sort().join('');
+        var crossesResult = crosses.join('').match(/\d+/g).sort().join('');
+        if (osResult.includes("012") || osResult.includes("345") || osResult.includes("678") || osResult.includes("036") || osResult.includes("147") || osResult.includes("258") || osResult.includes("048") || osResult.includes("246")) {
+            alert('os wins');
+        } else {
+            if (crossesResult.includes("012") || crossesResult.includes("345") || crossesResult.includes("678") || crossesResult.includes("036") || crossesResult.includes("147") || crossesResult.includes("258") || crossesResult.includes("048") || crossesResult.includes("246")) {
+                alert('crosses wins');
+            }   
+            else {
+                if (round === 8) {
+                    alert('draw');
+                }
+            }
+        }   
+    }
 })
