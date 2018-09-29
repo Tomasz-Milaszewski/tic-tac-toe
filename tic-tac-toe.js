e228da095f2cca46e972cc7e74034db114cdb23f
Tomek_M <tomasz.milaszewski@gmail.com>
2018-09-29 15:40:10 +0200

    adds infopanel and changes styling

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,16 +10,16 @@ document.addEventListener('click', function(event) {
       return;
     }
     // validation if click is on empty cell
-    if (clicked.innerText === 'x' || clicked.innerText === 'o') {
+    if (clicked.innerText === 'X' || clicked.innerText === 'O') {
         return;
     }
     // adds clicked elements number classes to their result arrays (os and crosses) and adds content
     if (round % 2 === 0) {
         os.push(clicked.className);
-        clicked.innerText = 'o';
+        clicked.innerText = 'O';
     } else {
         crosses.push(clicked.className);
-        clicked.innerText = 'x';
+        clicked.innerText = 'X';
     }
     // counts round number            
     round += 1;
@@ -28,14 +28,14 @@ document.addEventListener('click', function(event) {
         var osResult = os.join('').match(/\d+/g).sort().join('');
         var crossesResult = crosses.join('').match(/\d+/g).sort().join('');
         if (winningTable.some(code => osResult.includes(code)) === true) {
-            setTimeout(function() { alert('os wins'); }, 10);
+            setTimeout(function() { alert('os wins'); window.location.reload(); }, 10);
         } else {
             if ((winningTable.some(code => crossesResult.includes(code))) === true) {
-                setTimeout(function() { alert('crosses wins'); }, 10);
+                setTimeout(function() { alert('crosses wins'); window.location.reload(); }, 10);
             }   
             else {
                 if (round === 9) {
-                    setTimeout(function() { alert('draw'); }, 10);
+                    setTimeout(function() { alert('draw'); window.location.reload(); }, 10);
                 }
             }
         }   
