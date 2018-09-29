f99df9660c8532df6c3e6b967869ab4f92b1a898
Tomek_M <tomasz.milaszewski@gmail.com>
2018-09-29 18:11:08 +0200

    adds win function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,15 +1,15 @@
 var round = 0; //os are starting
 var crosses = [];
 var os = [];
-var winningTable = ["012","345","678","036","147","258","048","246"];
+var winningTable = ["012", "345", "678", "036", "147", "258", "048", "246"];
 var infoPanel = document.querySelector('.infoPanel');
 var infoPanelText = document.querySelector('span');
 
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     var clicked = event.target;
     // validation if click is on board
     if (!clicked.classList.contains('cell')) {
-      return;
+        return;
     }
     // validation if click is on empty cell
     if (clicked.innerText === 'X' || clicked.innerText === 'O') {
@@ -27,44 +27,37 @@ document.addEventListener('click', function(event) {
     }
     // counts round number            
     round += 1;
-    console.log(round);
     // winning condition
     if (round >= 5) {
         var osResult = os.join('').match(/\d+/g).sort().join('');
         var crossesResult = crosses.join('').match(/\d+/g).sort().join('');
-        console.log(osResult);
-        console.log(crossesResult);
-        if (winningTable.some(code => osResult.includes(code[0]) && osResult.includes(code[1])&& osResult.includes(code[2])) === true) {
-            infoPanelText.innerText = 'O wins';
-            infoPanel.style.backgroundColor = 'lightgreen';
-            setTimeout(function() {
+        var results = [osResult, crossesResult];
+        var signs = ['O', 'X']
+        round % 2 === 0 ? win(results[1],signs[1]) : win(results[0],signs[0]) 
+    }
+})
+
+function win(signResult, sign) {
+    if (winningTable.some(code => signResult.includes(code[0]) && signResult.includes(code[1]) && signResult.includes(code[2])) === true) {
+        infoPanelText.innerText = sign + ' wins';
+        infoPanel.style.backgroundColor = 'lightgreen';
+        setTimeout(function () {
+            infoPanelText.innerText = 'Play again';
+            infoPanel.style.backgroundColor = 'lightgrey';
+            infoPanel.style.cursor = 'pointer';
+            infoPanel.addEventListener('click', function () { window.location.reload() })
+        }, 1000)
+    } else {
+        if (round === 9) {
+            infoPanelText.innerText = 'Draw';
+            infoPanel.style.backgroundColor = 'lightsalmon';
+            setTimeout(function () {
                 infoPanelText.innerText = 'Play again';
                 infoPanel.style.backgroundColor = 'lightgrey';
                 infoPanel.style.cursor = 'pointer';
-                infoPanel.addEventListener('click', function(){window.location.reload()})
-                }, 1000);
-        } else {
-            if (winningTable.some(code => crossesResult.includes(code[0]) && crossesResult.includes(code[1])&& crossesResult.includes(code[2])) === true) {
-                infoPanelText.innerText = 'X wins';
-                infoPanel.style.backgroundColor = 'lightgreen';
-                setTimeout(function() {
-                    infoPanelText.innerText = 'Play again';
-                    infoPanel.style.backgroundColor = 'lightgrey';
-                    infoPanel.style.cursor = 'pointer';
-                    infoPanel.addEventListener('click', function(){window.location.reload()})
-                }, 1000);
-            } else {
-                if (round === 9) {
-                    infoPanelText.innerText = 'Draw';
-                    infoPanel.style.backgroundColor = 'lightsalmon';
-                    setTimeout(function() {
-                        infoPanelText.innerText = 'Play again';
-                        infoPanel.style.backgroundColor = 'lightgrey';
-                        infoPanel.style.cursor = 'pointer';
-                        infoPanel.addEventListener('click', function(){window.location.reload()})
-                    }, 1000);
-                }
-            }
-        }   
+                infoPanel.addEventListener('click', function () { window.location.reload() })
+            }, 1000);
+        }
     }
-})
+}
+    
